365fc2a5f9d0eef28ac3b4d849c6bbd71398e302
Jazzp1 <jazzprograms@gmail.com>
2022-10-19 12:54:00 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main-program entry point.
+ * Return: 0 no errors, non zero if erro.
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; <= '9'; i++)
+	{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
+
