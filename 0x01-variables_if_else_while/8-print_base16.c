18470e9a77d24eeac889144173ddb64250801b46
Jazzp1 <jazzprograms@gmail.com>
2022-10-20 23:59:59 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-
 /**
  * main-program entry point.
- * Return: 0 
+ * Return: 0 if no error, non zero if error.
  */
 int main(void)
 {
-	int i;
-	char hexvalues[] = "01234";
+ int i;
+ char hexvalues[] = "0123456789abcdef";
 
-	for (i = 0; i <= 16; i++);
-	{
-		putchar(hexvalues[i]);
-	}
-	putchar('\n');
-	return (0);
+ for (i = 0; i < 16; i++)
+ {
+  putchar(hexvalues[i]);
+ }
+ putchar('\n');
+ return (0);
 }
