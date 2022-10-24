3897fe92dab9d397c0b2a592c8aaf9080d082874
Jazzp1 <jazzprograms@gmail.com>
2022-10-24 00:41:24 -0700

    No description found for return value of _abs _abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - computes the abs value of an int
+ * @n: the int in question
+ *
+ * REtur: the abs of tne int.
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
