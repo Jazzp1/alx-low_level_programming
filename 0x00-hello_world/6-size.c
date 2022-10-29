3aa4d49428104f2dc1bda413327eeab9ef2cc579
Jazzp1 <jazzprograms@gmail.com>
2022-10-29 14:31:51 -0700

    0-mainc

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  * main - print out sizes of data types in C
  * code by Jazz programs
- * Return:
+ * Return: 0
 */
 int main(void)
 {
@@ -12,10 +12,11 @@ int main(void)
 	long long int d;
 	float f;
 
-	prinf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-	prinf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-        prinf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	prinf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	prinf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("size of an int: %lu byte(s)\n," (unsigned long)sizeof(b));
+	printf("size of a long int: %lu byte(s)\n," (unsigned long)sizeof(c));
+	printf("size of a long |long int: %lu byte(s)\n," (unsigned long)sizeof(d));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
+
 }
